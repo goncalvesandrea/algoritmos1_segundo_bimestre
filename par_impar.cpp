a5f237d4b3a17ee8e8a905db26bd2e62c9f805dd
goncalvesandrea <82242836+goncalvesandrea@users.noreply.github.com>
2021-05-28 19:39:21 -0300

    Add files via upload

---
+++ par_impar.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	
+	int num = 0;
+	
+	cout << "Digite um numero: \n";
+	cin >> num;
+	
+	if(num % 2 == 0){
+		cout << "Esse numero e par!" << "\n";
+	}else {
+		cout << "Esse numero e impar!" << "\n";
+	}
+	
+	system ("pause");
+	
+	return 0;
+	
+}
